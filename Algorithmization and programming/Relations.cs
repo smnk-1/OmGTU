1a3242ed1ac0915e1030ebfa43eda8ff7a572ef3
smnk-1 <144467094+smnk-1@users.noreply.github.com>
2024-02-14 11:05:29 +0600

    Update Relations.cs

--- Algorithmization and programming/Relations.cs
+++ Algorithmization and programming/Relations.cs
@@ -26,10 +26,14 @@ class Program
 	static void Main()
 	{	
 	    Relations user_1 = new Relations("Mark", "0001", new ArrayList {"0002"});
-		Relations user_2 = new Relations("Tom", "0002", new ArrayList{});
+		Relations user_2 = new Relations("Tom", "0002", new ArrayList{"0003", "0004"});
+		Relations user_3 = new Relations("N", "0003", new ArrayList{});
+		Relations user_4 = new Relations("D", "0004", new ArrayList{});
 	    ArrayList All = new ArrayList();
 		All.Add(user_1);
 		All.Add(user_2);
+		All.Add(user_3);
+		All.Add(user_4);
 		
 	    void Check_pod(Relations user)
 		{
@@ -37,8 +41,10 @@ class Program
 			{
 			    foreach(string i in user.Podchin)
 				{	
-				    Console.WriteLine(i.Id);
-					Check_pod(i);
+				    foreach(Relations us in All)
+					{
+					    if(us.Id == i){Console.WriteLine(i);Check_pod(us);}
+					}
 				}
 			}
 		}
