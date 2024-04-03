c6248090bb3840b4b0923d10fe8002debee7b0c5
smnk-1 <144467094+smnk-1@users.noreply.github.com>
2024-04-03 16:43:12 +0600

    Update PhoneNumbers2av2.cs

--- Algorithmization and programming/Semester 2/PhoneNumbers2av2.cs
+++ Algorithmization and programming/Semester 2/PhoneNumbers2av2.cs
@@ -23,35 +23,34 @@ namespace PhoneNubmersTask2
         }
 
         public void AddCall(string number2, string date)
-        {    
-			bool check = false;
+        {
+            bool check = false;
             foreach (Date d in calldata)
             {
-                if(d.date == date) { d.AddCall(number2); check = true;}
+                if (d.date == date) { d.AddCall(number2); check = true; }
             }
-            if(check == false)
+            if (check == false)
             {
                 Date new_d = new Date(date);
                 new_d.AddCall(number2);
                 calldata.Add(new_d);
             }
-    
         }
-        
+
         public void MostFrequent()
         {
-			foreach(Date data in calldata)
+            foreach (Date data in calldata)
             {
-				int maxvalue = data.calls.Values.Max();
-                foreach(string k in data.calls.Keys)
+                int maxvalue = data.calls.Values.Max();
+                foreach (string k in data.calls.Keys)
                 {
-					if(data.calls[k] == maxvalue)
+                    if (data.calls[k] == maxvalue)
                     {
-						Console.WriteLine($"{data.date}: {k}");
-					}
-				}
-			}
-		}
+                        Console.WriteLine($"{data.date}: {k}");
+                    }
+                }
+            }
+        }
     }
 
     class Date
@@ -64,66 +63,63 @@ namespace PhoneNubmersTask2
         }
         public void AddCall(string number_call_to)
         {
-            if(calls.ContainsKey(number_call_to))
+            if (calls.ContainsKey(number_call_to))
             {
                 calls[number_call_to] += 1;
             }
-            else 
+            else
             {
                 calls.Add(number_call_to, 1);
             }
         }
     }
 
-
     internal class Program
     {
         static void Main(string[] args)
         {
             List<Abonent> abonents = new List<Abonent>();
-            
+
             Console.WriteLine("Writer number_from number_to date(dd.mm.yyyy) duration OR END");
-            while(true)
-            {    
-				string[] inputs = Console.ReadLine().Split(' ');
-                if(inputs.Length != 4 && inputs[0] != "END") { Console.WriteLine("Input error"); }
-                else if(inputs[0] == "END"){break;}
+            while (true)
+            {
+                string[] inputs = Console.ReadLine().Split(' ');
+                if (inputs.Length != 4 && inputs[0] != "END") { Console.WriteLine("Input error"); }
+                else if (inputs[0] == "END") { break; }
                 else
-                {    
-				    string number_from = inputs[0];
+                {
+                    string number_from = inputs[0];
                     string number_to = inputs[1];
                     string call_date = inputs[2];
                     string call_duration = inputs[3];
                     bool flag = false;
-                    
-                    foreach(Abonent user in abonents)
-                    {    
-					    if(user.number == number_from)
+
+                    foreach (Abonent user in abonents)
+                    {
+                        if (user.number == number_from)
                         {
-						    user.AddCall(number_to, call_date);
+                            user.AddCall(number_to, call_date);
                             flag = true;
                             break;
-					    }
-				    }
-                    if(flag == false)
+                        }
+                    }
+                    if (flag == false)
                     {
-					    Abonent new_abonent = new Abonent(number_from);
-					    abonents.Add(new_abonent);
+                        Abonent new_abonent = new Abonent(number_from);
+                        abonents.Add(new_abonent);
                         new_abonent.AddCall(number_to, call_date);
                     }
                 }
-			}
+            }
             Console.Write("Needed number: ");
             string needed_number = Console.ReadLine();
-            foreach(Abonent user in abonents)
+            foreach (Abonent user in abonents)
             {
-				if(user.number == needed_number)
+                if (user.number == needed_number)
                 {
-					user.MostFrequent();
-				}
-			}
-            
+                    user.MostFrequent();
+                }
+            }
         }
-        
     }
 }
