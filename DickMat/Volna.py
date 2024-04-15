936cc92b8999f93cf895a2649b9f72daa7e23e42
smnk-1 <144467094+smnk-1@users.noreply.github.com>
2024-04-15 09:19:06 +0600

    Update Volna.py

--- DickMat/Volna.py
+++ DickMat/Volna.py
@@ -1,13 +1,5 @@
 import random
 
-# def count_zeros():
-#     global matrix
-#     count = 0
-#     for x in range(0, len(matrix)):
-#         count += matrix[i].count(0)
-#     return count
-#
-#
 matrix = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
@@ -18,9 +10,9 @@ matrix = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0]
 ]
-start = [0, 0]
-finish = [7, 6]
-
+start = [3, 3]
+finish = [0, 6]
+#random
 for i in range(0, len(matrix)):
     for j in range(0, len(matrix[i])):
         rand = random.random()
@@ -28,27 +20,49 @@ for i in range(0, len(matrix)):
             if not (i == start[0] and j == start[1]):
                 if not (i == finish[0] and j == finish[1]):
                     matrix[i][j] = -1
-
-for i in range(0, len(matrix)):
-    for j in range(0, len(matrix[i])):
-        print(matrix[i][j], end='\t')
+#print
+def output():
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
+            print(matrix[i][j], end='\t')
+        print(' ')
     print(' ')
+    
+output()
 
-start = 1
+matrix[start[0]][start[1]] = 1
+
+output()
+
 check = True
+
 while check:
     check = False
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
-            if matrix[i][j] >= 0:
+            if matrix[i][j] > 0:
+                # print(matrix[i][j])
                 # current_point - matrix[i][j]
                 for y in range(i-1, i + 2):
                     for x in range(j - 1, j + 2):
                         if 0 <= y <= 7 and 0 <= x <= 6:
+                                
                             # neighbour - matrix[y][x]
-                            # to do if 0 - +1, else - min
+                            if matrix[y][x] == 0:
+                                matrix[y][x] = matrix[i][j]+1
+                                check = True
+                                # output()
+                            if matrix[y][x] >= 0:
+                                if matrix[i][j] + 1 < matrix[y][x]:
+                                    matrix[x][y] = matrix[i][j] + 1
+                                    check = True
+                                    # output()
 
 for i in range(0, len(matrix)):
     for j in range(0, len(matrix[i])):
         print(matrix[i][j], end='\t')
     print(' ')
+if(matrix[finish[0]][finish[1]])!= 0:
+    print(matrix[finish[0]][finish[1]])
+else:
+    print('impossible')
